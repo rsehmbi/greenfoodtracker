1028897be461343bce7acda72fb3b8bb34dade1a
ohsu <orion_hsu@sfu.ca>
2018-10-19 13:29:03 -0700

    attempt to add xml files #3

---
+++ app/src/main/java/com/t/teamten/greenfoodtracker/MainActivity.java
@@ -0,0 +1,68 @@
+package com.t.teamten.greenfoodtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.t.teamten.greenfoodtracker.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MAX_DROPDOWNS = 10;
+    private TextView mIntroText;
+    private Switch mDataEntrySwapSwitch;
+    private ArrayList<Spinner> mSpinnerArray;
+    private Button mResultsButton;
+    private static final int SET_TO_NUMBER = 1;
+    private static final int SET_TO_PERCENT = 0;
+    private int switchStatus = SET_TO_PERCENT;
+    // for use in determining how data is entered by the user
+    private ArrayList<Spinner> populateSpinnerArray() {
+        ArrayList<Spinner> populatedArray = new ArrayList<>();
+
+
+        return populatedArray;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSpinnerArray = new ArrayList<>();
+
+        mResultsButton = findViewById(R.id.resultsButton);
+        mResultsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //submit result function, load up the values in each and send it to the next activity.
+            }
+        });
+
+        mIntroText = findViewById(R.id.introInstructionText);
+        mDataEntrySwapSwitch = findViewById(R.id.dataEntrySwapSwitch);
+        mDataEntrySwapSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mIntroText.setText(R.string.intro_description_percentage);
+                    switchStatus = SET_TO_PERCENT;
+                }
+                else {
+                    mIntroText.setText(R.string.intro_description_number);
+                    switchStatus = SET_TO_NUMBER;
+                }
+            }
+        });
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.food_array, android.R.layout.simple_spinner_dropdown_item);
+        for(Spinner currentSpinner : mSpinnerArray) { //sets the items to be listed in each drop down menu
+            currentSpinner.setAdapter(adapter);
+        }
+    }
+}
