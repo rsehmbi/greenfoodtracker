6135f0a5997f9f6b333adcc55a409db4c46fd2c1
rsehmbi <ramanpreet_singh_sehmbi@sfu.ca>
2018-10-23 15:00:29 -0700

    Adds the Button and other requirements.

--- app/src/main/java/com/t/teamten/greenfoodtracker/LoginUser.java
+++ app/src/main/java/com/t/teamten/greenfoodtracker/LoginUser.java
@@ -1,15 +1,37 @@
 package com.t.teamten.greenfoodtracker;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginUser extends AppCompatActivity {
 
-    private int User;
+    EditText Name;
+    EditText Age;
+    private Button Login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
-        User = 5;
+
+        Name = (EditText) findViewById(R.id.NameID);
+        Age =(EditText)findViewById(R.id.AgeID);
+        Login = (Button)findViewById(R.id.Login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NextPage();
+            }
+        });
     }
+    public void NextPage()
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity (intent);
+    }
+
 }
