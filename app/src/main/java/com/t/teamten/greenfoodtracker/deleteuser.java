a51fd52ab4d5a8550b76da2bb647381a05708044
rs <rsehmbi@sfu.ca>
2018-11-05 23:00:47 -0800

    Changes the basic layout and add more features.

---
+++ app/src/main/java/com/t/teamten/greenfoodtracker/deleteuser.java
@@ -0,0 +1,59 @@
+package com.t.teamten.greenfoodtracker;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.t.teamten.greenfoodtracker.loginactivities.FirebaseLogin;
+
+public class deleteuser extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText user;
+    EditText password;
+    Button deleteaccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deleteuser);
+        mAuth = FirebaseAuth.getInstance();
+
+        user = (EditText) findViewById(R.id.nameid);
+        password = (EditText) findViewById(R.id.passwordtodelete);
+
+        deleteaccount = (Button) findViewById(R.id.DeleteID);
+        deleteaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                user.delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(deleteuser.this,"Userdeleted",Toast.LENGTH_LONG).show();
+                                    Intent movetoLoginScreen = new Intent(deleteuser.this,FirebaseLogin.class);
+                                    startActivity(movetoLoginScreen);
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+}
+
