1276d7752e4239105a5a042d1d8ea5c29286b3a5
rs <rsehmbi@sfu.ca>
2018-11-08 13:25:51 -0800

    "update"

--- app/src/main/java/com/t/teamten/greenfoodtracker/loginactivities/FirebaseLogin.java
+++ app/src/main/java/com/t/teamten/greenfoodtracker/loginactivities/FirebaseLogin.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +23,9 @@ public class FirebaseLogin extends AppCompatActivity {
     private EditText Username;
     private EditText Password;
 
+    String user;
+    String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,37 +34,36 @@ public class FirebaseLogin extends AppCompatActivity {
         Username =(EditText) findViewById(R.id.UserID);
         Password=(EditText) findViewById(R.id.Password);
         Auth = FirebaseAuth.getInstance();
-        /*btn = findViewById(R.id.no_signup);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v)
-            {
-                Intent intent = new Intent();
-                intent.setClass(FirebaseLogin.this,loginactivity.class);
-                startActivity(intent);
-            }
-        });*/
+
     }
 
     public void Registeruser(View view) {
-        final ProgressDialog progressDialog = ProgressDialog.show(FirebaseLogin.this,"Please wait..","Processing..",true);
+        user = Username.getText().toString();
+        password = Password.getText().toString();
 
-        Auth.signInWithEmailAndPassword(Username.getText().toString(),Password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                progressDialog.dismiss();
+        if (TextUtils.isEmpty(user) || TextUtils.isEmpty(password)) {
+            Toast.makeText(FirebaseLogin.this, "Please check Credentials!!", Toast.LENGTH_SHORT).show();
 
-                if(task.isSuccessful())
-                {
-                    Intent movetoHomescreen = new Intent(FirebaseLogin.this, HomeScreen.class);
-                    startActivity(movetoHomescreen);
-                    Toast.makeText(FirebaseLogin.this, "Succesfull", Toast.LENGTH_SHORT).show();
+        } else {
+            final ProgressDialog progressDialog = ProgressDialog.show(FirebaseLogin.this, "Please wait..", "Processing..", true);
 
-                }else{
-                    Toast.makeText(FirebaseLogin.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+            Auth.signInWithEmailAndPassword(Username.getText().toString(), Password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    progressDialog.dismiss();
+
+                    if (task.isSuccessful()) {
+                        Intent movetoHomescreen = new Intent(FirebaseLogin.this, HomeScreen.class);
+                        startActivity(movetoHomescreen);
+                          Toast.makeText(FirebaseLogin.this, "Succesfull", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(FirebaseLogin.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
 
-        });
+            });
+        }
     }
 
     public void movetoregisterationpage(View view) {
---
+++ app/src/main/java/com/t/teamten/greenfoodtracker/settingsforuser.java
@@ -0,0 +1,53 @@
+package com.t.teamten.greenfoodtracker;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.t.teamten.greenfoodtracker.loginactivities.FirebaseLogin;
+import com.t.teamten.greenfoodtracker.loginactivities.aboutactivity;
+
+public class settingsforuser extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settingsactivity);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    public void move(View view) {
+        Intent movetoaboutactivity = new Intent(settingsforuser.this,aboutactivity.class);
+        startActivity(movetoaboutactivity);
+    }
+
+    public void signoutprofile(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent movetologin = new Intent(settingsforuser.this, FirebaseLogin.class);
+        startActivity(movetologin);
+        finish();
+    }
+
+    public void deleteid(View view) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user.delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(settingsforuser.this, "Userdeleted", Toast.LENGTH_LONG).show();
+                            Intent movetoLoginScreen = new Intent(settingsforuser.this, FirebaseLogin.class);
+                            startActivity(movetoLoginScreen);
+                        }
+                    }
+                });
+    }
+}
